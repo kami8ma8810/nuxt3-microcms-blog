a1589c7cb18f87675079a7e16e8ca03c29972d00
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2023-06-17 15:53:41 +0900

    インストール

---
+++ microcms-blog/pages/index.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script setup lang="ts">
+import { Blog } from "~~/types/Blog";
+
+const { data } = await useMicroCMSGetList<Blog>({
+  endpoint: "blogs",
+});
+console.log(data);
+</script>
